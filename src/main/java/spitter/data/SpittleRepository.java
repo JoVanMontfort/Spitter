7e3195780b37c09f0215eb5ff1cfb2b5c1e1dac5
JoVM <jo.vanmontfort@cheops.be>
2019-07-08 15:40:43 +0200

    Added findById

--- src/main/java/spitter/data/SpittleRepository.java
+++ src/main/java/spitter/data/SpittleRepository.java
@@ -1,6 +1,7 @@
 package spitter.data;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -11,5 +12,7 @@ public interface SpittleRepository extends JpaRepository<Spittle, Long> {
 	List<Spittle> findAll();
 
 	Spittle findByMessage(String message);
+
+	Optional<Spittle> findById(Long id);
 
 }
